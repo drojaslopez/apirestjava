3de832b45ecab1717df0cd22e46c211e3d48f634
unknown <drojaslopez.ing@gmail.com>
2025-05-17 20:14:58 -0400

    cambien en los endpoint

--- src/main/java/com/drojaslopez/apirest/Persona/PersonaController.java
+++ src/main/java/com/drojaslopez/apirest/Persona/PersonaController.java
@@ -3,55 +3,49 @@ package com.drojaslopez.apirest.Persona;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.NoSuchElementException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-
 
 @RestController
 @RequestMapping("/persona")
-@RequiredArgsConstructor
 public class PersonaController {
-
-    private final PersonaService personaService;
+    
+    @Autowired
+    private PersonaService personaService;
 
     @PostMapping("/crear")
-    public void crearPersona(@RequestBody Persona persona) {
+    public ResponseEntity<Persona> crearPersona(@RequestBody Persona persona) {
         try {
             personaService.savePersona(persona);
             System.out.println("Persona creada: " + persona);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         } catch (Exception e) {
-            System.out.println("Error al crear persona: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
-
-
-
-
-
-
-
-        
     }
-    
+
     @GetMapping("/listar")
-    public Iterable<Persona> listarPersonas() {
-        try {           
-            return personaService.getAllPersonas();
+    public ResponseEntity<Iterable<Persona>> listarPersonas() {
+        try {
+            Iterable<Persona> personas = personaService.getAllPersonas();
+            return ResponseEntity.ok(personas);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         } catch (Exception e) {
-            System.out.println("Error al listar personas: " + e.getMessage());}
-            return null;
-            
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
+    }
 
     @GetMapping("/{rut}")
     public ResponseEntity<Persona> obtenerPersonaPorRut(@PathVariable String rut) {
@@ -67,6 +61,5 @@ public class PersonaController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
-    
 
 }
